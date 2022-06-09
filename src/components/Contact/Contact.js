6dc8fec450f5177beab7af12e18b4309350d37bf
shahnmah <shahn.mah@gmail.com>
2022-06-09 21:21:19 +0600

    email send added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,24 @@
 import { Controls, Player } from '@lottiefiles/react-lottie-player';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        
+        e.preventDefault();
+       
+        emailjs.sendForm('service_ppicngi', 'template_v5r8m38', form.current, '98eZXE4kLrpSIU2TB')
+          .then((result) => {
+              console.log(result.text);
+              
+          }, (error) => {
+              console.log(error.text);
+          });
+      
+    };
+  
     return (
         <div className='contact py-5' style={{ backgroundColor: '#FBFBFB' }}>
             <div className="container">
@@ -22,23 +38,20 @@ const Contact = () => {
                         </Player>
                     </div>
                     <div className="col-lg-6 col-md-6 com-sm-12 col-12">
-                        <Form className='mt-5'>
+                        <form className='mt-5'  ref={form} onSubmit={sendEmail}>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Control type="name" placeholder="Your Name" />
+                                <Form.Control name="user_name" placeholder="Your Name" required/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Control type="email" placeholder="Your Email" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Control type="name" placeholder="Your Subject" />
+                                <Form.Control name="user_email" type="email" placeholder="Your Email" required/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Control as="textarea" rows={3} placeholder="Your Message"/>
+                                <Form.Control name="message" as="textarea" rows={3} placeholder="Your Message" required/>
                             </Form.Group>
                             <Button className='primary-button w-100' type="submit">
                                 Send
                             </Button>
-                        </Form>
+                        </form>
                     </div>
                 </div>
             </div>
